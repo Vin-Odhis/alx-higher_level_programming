b69c08e1be516e5c21fd70f0bd54527782f3b552
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2023-04-02 23:35:07 +0300

    comments

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-Same as Task 0 but using request package
-"""
+""" Uses requests module to fetch URL status"""
 
 import requests
 
-
-if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+if __name__ == '__main__':
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:\n\t- type: {}\n\t- content: {}"
+          .format(type(response.text), response.text))
